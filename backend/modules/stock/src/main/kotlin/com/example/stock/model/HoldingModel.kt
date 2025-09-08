be1db7a2f6df5cf281b06aadc73dbbde719e4075
ijikeman <keiji.ue@gmail.com>
2025-09-09 00:08:47 +0900

    add holding model

---
+++ backend/modules/stock/src/main/kotlin/com/example/stock/model/HoldingModel.kt
@@ -0,0 +1,53 @@
+package com.example.stock.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import jakarta.persistence.GenerationType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.Column
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.NotBlank
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.JoinColumn
+
+/* 
+    holding {
+        UUID id PK
+        UUID owner_id FK "株式を保有するユーザーID"
+        UUID stock_id FK "保有銘柄ID"
+        integer quantity "現在保有している株数"
+        decimal average_price "購入にかかった平均取得単価"
+        timestamp updated_at "最終更新日時"
+    }
+*/
+@Entity
+@Table(name = "holding")
+data class Holding (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    val owner: Owner,
+
+    @ManyToOne
+    @JoinColumn(name = "stock_id")
+    val stock: Stock,
+
+    @field:NotBlank(message = "数量は必須です")
+    @field:Pattern(regexp = "^[0-9]+$", message = "数量は数字のみで構成される必要があります")
+    @Column(name = "quantity", nullable = false)
+    val quantity: Int = 0,
+
+    @field:NotBlank(message = "平均取得単価は必須です")
+    @field:Pattern(regexp = "^[0-9]+$", message = "平均取得単価は数字のみで構成される必要があります")
+    @Column(name = "average_price", nullable = false)
+    val average_price: Double = 0.0,
+
+    @field:NotBlank(message = "最終更新日時は必須です")
+    @field:Pattern(regexp = "^[0-9]+$", message = "最終更新日時は数字のみで構成される必要があります")
+    @Column(name = "updated_at")
+    val updated_at: java.time.LocalDate? = null
+)
---
+++ backend/modules/stock/src/main/kotlin/com/example/stock/repository/HoldingRepository.kt
@@ -0,0 +1,11 @@
+package com.example.stock.repository
+
+import com.example.stock.model.Holding
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface HoldingRepository : JpaRepository<Holding, Int> {
+    // owner_idで検索
+    fun findByOwnerId(owner_id: Int): Holding?
+}
